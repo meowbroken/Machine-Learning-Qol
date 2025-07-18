87150a6ad10b5b6d51e395f10fb04025c9890bf3
Philip James Olbedencia <meowbroken@gmail.com>
2025-07-19 00:21:22 +0800

    Changes in urls

--- MlBackend/urls.py
+++ MlBackend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/qualityoflife/', include('qualityOfLife.urls')),
+    path('api/', include('qualityOfLife.urls')),
 
 ]
